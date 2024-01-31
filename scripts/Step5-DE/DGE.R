bbd79522d44a8348a475aad2a9bdde529157dbd8
Michael Jopiti <michaeljopiti@vpn-client-43-102.unifr.ch>
2024-01-31 02:30:31 +0100

    final adjustments + finally an understandable questions' answers file

--- scripts/Step5-DE/DGE.R
+++ scripts/Step5-DE/DGE.R
@@ -27,7 +27,7 @@ t2g <- read.csv("../../expression/target_mapping.txt", header = FALSE)
 colnames(t2g) <-  c("target_id", "gene_name", "gene_type")
 
 #preparation of sleuth object
-so <- sleuth_prep(s2c, target_mapping = t2g)
+so <- sleuth_prep(s2c, target_mapping = t2g, extra_bootstrap_summary=T)
 so <- sleuth_fit(so, ~condition, 'full')
 so <- sleuth_fit(so, ~1, 'reduced')
 so <- sleuth_wt(so, 'conditionHoloclonal')
@@ -44,7 +44,6 @@ unknown_genes_table <- result_table[is.na(result_table$gene_type),]
 significant_genes <- dplyr::filter(unknown_genes_table, qval <=0.05)
 head(significant_genes, 20)
 
-
 #volcano plot for data visualization
 EnhancedVolcano(result_table,
                 lab=result_table$target_id,
@@ -58,3 +57,48 @@ EnhancedVolcano(known_genes_table,
                 lab=known_genes_table$gene_name,
                 x="b",
                 y="qval")
+
+# Select the top 20 most significant genes by q-value
+top_genes <- known_genes_table[order(known_genes_table$qval)[1:20], ]
+
+# Create the EnhancedVolcano plot with labels for the top 20 genes
+report_graph <- EnhancedVolcano(known_genes_table,
+                                lab = ifelse(known_genes_table$gene_name %in% top_genes$gene_name, as.character(known_genes_table$gene_name), ""),
+                                x = "b",
+                                y = "qval",
+                                pCutoff = 0.001,
+                                FCcutoff = 0.05,
+                                shape = 2,
+                                labSize = 3.0,
+                                title = "Volcano plot for Holoclonal DE genes",
+                                titleLabSize = 15.0,
+                                subtitle = "Annotated Genes",
+                                subtitleLabSize = 10.0,
+                                legendLabSize = 7.0,
+                                legendIconSize = 3.0,
+                                drawConnectors = TRUE) + coord_flip()
+report_graph
+
+## Individual plots for single gene testing
+### genes taken from paper or most differential expressed table
+#CDH17 from paper for EMT is over present in my analysis as well
+plot_bootstrap(so, "ENST00000441892.6", units = "est_counts", color_by = "condition")
+#EpCAM from paper for CSC marker  is over present in my analysis as well
+plot_bootstrap(so, "ENST00000263735.9", units = "est_counts", color_by = "condition")
+#TP53
+plot_bootstrap(so, "ENST00000505014.5", units = "est_counts", color_by = "condition")
+plot_bootstrap(so, "ENST00000508793.6", units = "est_counts", color_by = "condition")
+plot_bootstrap(so, "ENST00000571370.2", units = "est_counts", color_by = "condition")
+plot_bootstrap(so, "ENST00000618944.4", units = "est_counts", color_by = "condition")
+plot_bootstrap(so, "ENST00000619485.4", units = "est_counts", color_by = "condition")
+plot_bootstrap(so, "ENST00000620739.4", units = "est_counts", color_by = "condition")
+plot_bootstrap(so, "ENST00000622645.4", units = "est_counts", color_by = "condition")
+plot_bootstrap(so, "ENST00000635293.1", units = "est_counts", color_by = "condition")
+plot_bootstrap(so, "ENST00000622645.4", units = "est_counts", color_by = "condition")
+
+#PCA plot
+plot_pca(so, color_by = 'condition')
+#quality check for amount of counts between conditions
+plot_group_density(so, use_filtered = TRUE, units = "est_counts",
+                   trans = "log", grouping = setdiff(colnames(so$sample_to_covariates),
+                                                     "sample"), offset = 1)
